d212b28fb4d6ebd4c7b6006260d34a38eb7ac15c
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-22 12:30:31 +0300

    7 need tests

--- formatter.js
+++ formatter.js
@@ -1,21 +1,21 @@
 import _ from 'lodash';
 
-const buildDiff = (obj1, obj2, depth = 1) => {
+export const MakeStylishDiff = (obj1, obj2, depth = 1) => {
     const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
     
     const diffLines = keys.map((key) => {
         const currentDepth = '  '.repeat(depth);
         if (!_.has(obj1, key)) {
-            return `${currentDepth}+ ${key}: ${formatValue(obj2[key], depth)}`;
+            return `${currentDepth}+ ${key}: ${formatValue(obj2[key], depth + 1)}`;
         }
         if (!_.has(obj2, key)) {
-            return `${currentDepth}- ${key}: ${formatValue(obj1[key], depth)}`;
+            return `${currentDepth}- ${key}: ${formatValue(obj1[key], depth + 1)}`;
         }
         if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-            return `${currentDepth}  ${key}: ${buildDiff(obj1[key], obj2[key], depth + 1)}`;
+            return `${currentDepth}  ${key}: ${MakeStylishDiff(obj1[key], obj2[key], depth + 2)}`;
         }
         if (obj1[key] === obj2[key]) {
-            return `${currentDepth}  ${key}: ${formatValue(obj1[key], depth)}`;
+            return `${currentDepth}  ${key}: ${formatValue(obj1[key], depth + 2)}`;
         }
         return `${currentDepth}- ${key}: ${formatValue(obj1[key], depth)}\n${currentDepth}+ ${key}: ${formatValue(obj2[key], depth)}`;
     });
@@ -23,11 +23,11 @@ const buildDiff = (obj1, obj2, depth = 1) => {
     return `{\n${diffLines.join('\n')}\n${'  '.repeat(depth - 1)}}`;
 };
 
+
+
 const formatValue = (value, depth) => {
     if (_.isObject(value)) {
-        return buildDiff(value, value, depth + 2);
+        return MakeStylishDiff(value, value, depth + 2);
     }
     return value;
 };
-
-export default buildDiff;
