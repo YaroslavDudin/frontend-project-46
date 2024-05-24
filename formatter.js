2ec9bba1257b2154f7b4a731e8903cb8f8b9f070
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-24 15:07:04 +0300

    plain

--- formatter.js
+++ formatter.js
@@ -31,54 +31,51 @@ const makeStylishDiff = (obj1, obj2, depth = 1) => {
 };
 
 const makePlainDiff = (obj11, obj22) => {
-  const diffOutput = [];
-
-  const formatValue = (value) => {
-    if (_.isObject(value)) {
-      return '[complex value]';
-    }
-    if (typeof value === 'string') return `'${value}'`;
-    return value;
-  };
-
-  const traverseObject = (obj1, obj2, prefix = '') => {
-    Object.keys(obj1).forEach((key) => {
-      const fullKey = prefix ? `${prefix}.${key}` : key;
-      if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
-        diffOutput.push(`Property '${fullKey}' was removed`);
-      } else if (_.isEqual(obj1[key], obj2[key])) {
-        // Property exists in both objects and their values are equal
-        // No action needed
-      } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        traverseObject(obj1[key], obj2[key], fullKey);
-      } else {
-        diffOutput.push(`Property '${fullKey}' was updated. From ${formatValue(obj1[key])} to ${formatValue(obj2[key])}`);
+    const formatValue = (value) => {
+      if (_.isObject(value)) {
+        return '[complex value]';
       }
-    });
-
-    Object.keys(obj2).forEach((key) => {
-      if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
+      if (typeof value === 'string') return `'${value}'`;
+      return value;
+    };
+  
+    const traverseObject = (obj1, obj2, prefix = '') => {
+      const diffOutput = Object.keys(obj1).reduce((acc, key) => {
         const fullKey = prefix ? `${prefix}.${key}` : key;
-        diffOutput.push(`Property '${fullKey}' was added with value: ${formatValue(obj2[key])}`);
-      }
-    });
+        if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
+          return acc.concat(`Property '${fullKey}' was removed`);
+        } else if (_.isEqual(obj1[key], obj2[key])) {
+          return acc;
+        } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+          return traverseObject(obj1[key], obj2[key], fullKey).concat(acc);
+        } else {
+          return acc.concat(`Property '${fullKey}' was updated. From ${formatValue(obj1[key])} to ${formatValue(obj2[key])}`);
+        }
+      }, []);
+  
+      const newKeys = Object.keys(obj2).filter((key) => !Object.prototype.hasOwnProperty.call(obj1, key));
+      const newDiffOutput = newKeys.map((key) => {
+        const fullKey = prefix ? `${prefix}.${key}` : key;
+        return `Property '${fullKey}' was added with value: ${formatValue(obj2[key])}`;
+      });
+  
+      return diffOutput.concat(newDiffOutput).toSorted();
+    };
+  
+    return traverseObject(obj11, obj22).join('\n');
   };
-
-  traverseObject(obj11, obj22);
-
-  return diffOutput.sort().join('\n');
-};
-const formatter = (data1, data2, format) => {
-  switch (format) {
-    case 'stylish':
-      return makeStylishDiff(data1, data2);
-    case 'plain':
-      return makePlainDiff(data1, data2);
-    case 'json':
-      return JSON.stringify(data1, data2, null);
-    default:
-      throw new Error('invalid data');
-  }
-};
+  
+  const formatter = (data1, data2, format) => {
+    switch (format) {
+      case 'stylish':
+        return makeStylishDiff(data1, data2);
+      case 'plain':
+        return makePlainDiff(data1, data2);
+      case 'json':
+        return JSON.stringify(data1, data2, null);
+      default:
+        throw new Error('invalid data');
+    }
+  };
 
 export default formatter;
