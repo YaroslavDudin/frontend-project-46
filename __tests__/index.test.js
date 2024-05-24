d5a348c5af5dc5e7eb29db868a260e9dc128db45
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-24 14:32:44 +0300

    123

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,75 +1,77 @@
-import { describe, expect, it , test } from '@jest/globals';
+import {
+  describe, expect, it, test,
+} from '@jest/globals';
 import * as func from '../index.js';
 import genDiff from '../index.js';
- describe('genDiff', () => {
-   it('should be undefined', () => {
-     expect(func.genDiff).toBeUndefined();
-   });
- })
 
+describe('genDiff', () => {
+  it('should be undefined', () => {
+    expect(func.genDiff).toBeUndefined();
+  });
+});
 
- describe('genDiff', () => {
-     test('should generate the correct diff output with proper indentation', () => {
-         const obj1 = {
-          "common": {
-            "setting1": "Value 1",
-            "setting2": 200,
-            "setting3": true,
-            "setting6": {
-              "key": "value",
-              "doge": {
-                "wow": ""
-              }
-            }
+describe('genDiff', () => {
+  test('should generate the correct diff output with proper indentation', () => {
+    const obj1 = {
+      common: {
+        setting1: 'Value 1',
+        setting2: 200,
+        setting3: true,
+        setting6: {
+          key: 'value',
+          doge: {
+            wow: '',
           },
-          "group1": {
-            "baz": "bas",
-            "foo": "bar",
-            "nest": {
-              "key": "value"
-            }
+        },
+      },
+      group1: {
+        baz: 'bas',
+        foo: 'bar',
+        nest: {
+          key: 'value',
+        },
+      },
+      group2: {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+    };
+
+    const obj2 = {
+      common: {
+        follow: false,
+        setting1: 'Value 1',
+        setting3: null,
+        setting4: 'blah blah',
+        setting5: {
+          key5: 'value5',
+        },
+        setting6: {
+          key: 'value',
+          ops: 'vops',
+          doge: {
+            wow: 'so much',
           },
-          "group2": {
-            "abc": 12345,
-            "deep": {
-              "id": 45
-            }
-          }
-         };
-         
-         const obj2 = {
-          "common": {
-            "follow": false,
-            "setting1": "Value 1",
-            "setting3": null,
-            "setting4": "blah blah",
-            "setting5": {
-              "key5": "value5"
-            },
-            "setting6": {
-              "key": "value",
-              "ops": "vops",
-              "doge": {
-                "wow": "so much"
-              }
-            }
+        },
+      },
+      group1: {
+        foo: 'bar',
+        baz: 'bars',
+        nest: 'str',
+      },
+      group3: {
+        deep: {
+          id: {
+            number: 45,
           },
-          "group1": {
-            "foo": "bar",
-            "baz": "bars",
-            "nest": "str"
-          },
-          "group3": {
-            "deep": {
-              "id": {
-                "number": 45
-              }
-            },
-            "fee": 100500
-          }
-         };
- 
-  const expectedDiffOutput = `{
+        },
+        fee: 100500,
+      },
+    };
+
+    const expectedDiffOutput = `{
     common: {
     + follow: false
       setting1: Value 1
@@ -112,9 +114,7 @@ import genDiff from '../index.js';
       }
         fee: 100500
     }
-}`
-;
- 
-         expect(genDiff(obj1, obj2)).toEqual(expectedDiffOutput);
-     });
- });
+}`;
+    expect(genDiff(obj1, obj2)).toEqual(expectedDiffOutput);
+  });
+});
