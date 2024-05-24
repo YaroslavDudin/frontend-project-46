7e885c93a90ecec5cd67186b61ca9f73b7492e36
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-24 14:58:54 +0300

    plain

--- formatter.js
+++ formatter.js
@@ -1,31 +1,30 @@
-/* eslint-disable */
 import _ from 'lodash';
 
-const formatValue = (value, depth) => {
-  if (_.isObject(value)) {
-    return makeStylishDiff(value, value, depth + 1);
-  }
-  return value;
-};
 const makeStylishDiff = (obj1, obj2, depth = 1) => {
+  const formatValue1 = (value, depth1) => {
+    if (_.isObject(value)) {
+      return makeStylishDiff(value, value, depth1 + 1);
+    }
+    return value;
+  };
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
 
   const diffLines = keys.map((key) => {
     const currentDepth = '  '.repeat(depth);
     const indentation = '  '.repeat(depth + 1);
     if (!_.has(obj1, key)) {
-      return `${currentDepth}+ ${key}: ${formatValue(obj2[key], depth + 1, indentation)}`;
+      return `${currentDepth}+ ${key}: ${formatValue1(obj2[key], depth + 1, indentation)}`;
     }
     if (!_.has(obj2, key)) {
-      return `${currentDepth}- ${key}: ${formatValue(obj1[key], depth + 1, indentation)}`;
+      return `${currentDepth}- ${key}: ${formatValue1(obj1[key], depth + 1, indentation)}`;
     }
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return `${currentDepth}  ${key}: ${makeStylishDiff(obj1[key], obj2[key], depth + 2)}`;
     }
     if (obj1[key] === obj2[key]) {
-      return `${currentDepth}  ${key}: ${formatValue(obj1[key], depth + 1, indentation)}`;
+      return `${currentDepth}  ${key}: ${formatValue1(obj1[key], depth + 1, indentation)}`;
     }
-    return `${currentDepth}- ${key}: ${formatValue(obj1[key], depth + 1, indentation)}\n${currentDepth}+ ${key}: ${formatValue(obj2[key], depth + 1, indentation)}`;
+    return `${currentDepth}- ${key}: ${formatValue1(obj1[key], depth + 1, indentation)}\n${currentDepth}+ ${key}: ${formatValue1(obj2[key], depth + 1, indentation)}`;
   });
 
   return `{\n${diffLines.join('\n')}\n${'  '.repeat(depth - 1)}}`;
@@ -42,18 +41,18 @@ const makePlainDiff = (obj1, obj2) => {
     return value;
   };
 
-  const traverseObject = (obj1, obj2, prefix = '') => {
-    Object.keys(obj1).forEach((key) => {
+  const traverseObject = (obj11, obj22, prefix = '') => {
+    Object.keys(obj11).forEach((key) => {
       const fullKey = prefix ? `${prefix}.${key}` : key;
-      if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
+      if (!Object.prototype.hasOwnProperty.call(obj22, key)) {
         diffOutput.push(`Property '${fullKey}' was removed`);
-      } else if (_.isEqual(obj1[key], obj2[key])) {
+      } else if (_.isEqual(obj11[key], obj22[key])) {
         // Property exists in both objects and their values are equal
         // No action needed
-      } else if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        traverseObject(obj1[key], obj2[key], fullKey);
+      } else if (_.isPlainObject(obj11[key]) && _.isPlainObject(obj22[key])) {
+        traverseObject(obj11[key], obj22[key], fullKey);
       } else {
-        diffOutput.push(`Property '${fullKey}' was updated. From ${formatValue(obj1[key])} to ${formatValue(obj2[key])}`);
+        diffOutput.push(`Property '${fullKey}' was updated. From ${formatValue(obj11[key])} to ${formatValue(obj22[key])}`);
       }
     });
 
