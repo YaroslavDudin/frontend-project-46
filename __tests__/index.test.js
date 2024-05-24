40266ae5587d9d5007b3710c292ea8ea0146ad67
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-24 22:03:53 +0300

    check

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,120 @@
-import { describe, expect, it, test } from '@jest/globals';
-import formatter from '../formatter.js';
-import * as func from '../formatter.js';
-import * as example from '../__fixtures__/results.js';
+import {
+  describe, expect, it, test,
+} from '@jest/globals';
+import * as func from '../index.js';
+import genDiff from '../index.js';
 
- describe('genDiff1', () => {
-   it('should be undefined', () => {
-     expect(func.formatter).toBeUndefined();
-   });
- })
+describe('genDiff', () => {
+  it('should be undefined', () => {
+    expect(func.genDiff).toBeUndefined();
+  });
+});
 
+describe('genDiff1', () => {
+  test('should generate the correct diff output with proper indentation', () => {
+    const obj1 = {
+      common: {
+        setting1: 'Value 1',
+        setting2: 200,
+        setting3: true,
+        setting6: {
+          key: 'value',
+          doge: {
+            wow: '',
+          },
+        },
+      },
+      group1: {
+        baz: 'bas',
+        foo: 'bar',
+        nest: {
+          key: 'value',
+        },
+      },
+      group2: {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+    };
 
- describe('genDiff', () => {
-     test('should generate the correct diff output with proper indentation', () => {
+    const obj2 = {
+      common: {
+        follow: false,
+        setting1: 'Value 1',
+        setting3: null,
+        setting4: 'blah blah',
+        setting5: {
+          key5: 'value5',
+        },
+        setting6: {
+          key: 'value',
+          ops: 'vops',
+          doge: {
+            wow: 'so much',
+          },
+        },
+      },
+      group1: {
+        foo: 'bar',
+        baz: 'bars',
+        nest: 'str',
+      },
+      group3: {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    };
 
-        //  expect(formatter(example.obj1, example.obj2, 'stylish')).toEqual(example.expectedStylishOutput);
-         expect(formatter(example.obj1, example.obj2, 'plain')).toEqual(example.expectedPlainOutput);
-        //  expect(formatter(obj1, obj2, 'json')).toEqual(expectedJsonOutput);
-     });
- });
+    const expectedDiffOutput = `{
+    common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+        doge: {
+        - wow: 
+        + wow: so much
+      }
+        key: value
+      + ops: vops
+    }
+  }
+    group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+  - group2: {
+        abc: 12345
+        deep: {
+          id: 45
+      }
+    }
+  + group3: {
+        deep: {
+          id: {
+            number: 45
+        }
+      }
+        fee: 100500
+    }
+}`;
+    expect(genDiff(obj1, obj2)).toEqual(expectedDiffOutput);
+  });
+});
