af0bca758b657cca72dce047fc34d540b314335f
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-05-29 12:14:49 +0300

    check

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,120 +1,27 @@
 import {
   describe, expect, it, test,
 } from '@jest/globals';
-import * as func from '../index.js';
 import genDiff from '../index.js';
+import resultStylish from '../__fixtures__/result';
+import resultPlain from '../__fixtures__/resultPlain.js';
+import resultJSON from '../__fixtures__/resultJSON.js';
+import path from 'node:path';
 
-describe('genDiff', () => {
-  it('should be undefined', () => {
-    expect(func.genDiff).toBeUndefined();
-  });
-});
+const testList = [
+  'yml',
+  'json',
+];
 
-describe('genDiff1', () => {
-  test('should generate the correct diff output with proper indentation', () => {
-    const obj1 = {
-      common: {
-        setting1: 'Value 1',
-        setting2: 200,
-        setting3: true,
-        setting6: {
-          key: 'value',
-          doge: {
-            wow: '',
-          },
-        },
-      },
-      group1: {
-        baz: 'bas',
-        foo: 'bar',
-        nest: {
-          key: 'value',
-        },
-      },
-      group2: {
-        abc: 12345,
-        deep: {
-          id: 45,
-        },
-      },
-    };
+const resolvePath = (filePath) => path.resolve(process.cwd(), `__fixtures__/${filePath}`);
 
-    const obj2 = {
-      common: {
-        follow: false,
-        setting1: 'Value 1',
-        setting3: null,
-        setting4: 'blah blah',
-        setting5: {
-          key5: 'value5',
-        },
-        setting6: {
-          key: 'value',
-          ops: 'vops',
-          doge: {
-            wow: 'so much',
-          },
-        },
-      },
-      group1: {
-        foo: 'bar',
-        baz: 'bars',
-        nest: 'str',
-      },
-      group3: {
-        deep: {
-          id: {
-            number: 45,
-          },
-        },
-        fee: 100500,
-      },
-    };
+describe('gendiff', () => {
+  test.each(testList)('gendiff %s', (format) => {
+    const filepath1 = resolvePath(`file1.${format}`);
+    const filepath2 = resolvePath(`file2.${format}`);
 
-    const expectedDiffOutput = `{
-    common: {
-    + follow: false
-      setting1: Value 1
-    - setting2: 200
-    - setting3: true
-    + setting3: null
-    + setting4: blah blah
-    + setting5: {
-          key5: value5
-      }
-      setting6: {
-        doge: {
-        - wow: 
-        + wow: so much
-      }
-        key: value
-      + ops: vops
-    }
-  }
-    group1: {
-    - baz: bas
-    + baz: bars
-      foo: bar
-    - nest: {
-          key: value
-      }
-    + nest: str
-  }
-  - group2: {
-        abc: 12345
-        deep: {
-          id: 45
-      }
-    }
-  + group3: {
-        deep: {
-          id: {
-            number: 45
-        }
-      }
-        fee: 100500
-    }
-}`;
-    expect(genDiff(obj1, obj2)).toEqual(expectedDiffOutput);
+    expect(genDiff(filepath1, filepath2)).toEqual(resultStylish);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(resultStylish);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(resultPlain);
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(resultJSON);
   });
 });
